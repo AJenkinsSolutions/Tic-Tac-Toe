45cdfc8025192d450a81a8e7208db3e26914e9f0
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-05-26 18:10:39 -0500

    Code Refactor

--- main.py
+++ main.py
@@ -1,38 +1,14 @@
 def main():
-    #   Grid Structure
-    # margin
-    # row
-    # divide
-    # margin
-    # row
-    # divide
-    # margin
-    # row
-    # margin
-    DIVIDE = '_____|_____|_____'
-    MARGIN = '     |     |     '
-    a0 = '  -  '
-    b0 = '|  -  |'
-    c0 = '  -  '
-    row_0 = [a0, b0, c0]
-    #Row 1
-    a1 = '  -  '
-    b1 = '|  -  |'
-    c1 = '  -  '
-    #Row 2
-    a2 = '  -  '
-    b2 = '|  -  |'
-    c2 = '  -  '
-
-
     class Grid:
-        #Class Attributes
-        #All grids will have the same margin and divide this does not change
+        #   Class Attributes
+        #   All grids will have the same margin and divide
         margin = '     |     |     '
         divide = '_____|_____|_____'
-        #Instance Methods
+
+        #   Instance Attributes
         def __init__(self):
-            #Class Attributes
+
+            #   Class Attributes
             self.rows = {
                 'a0': '  -  ', 'b0': '|  -  |', 'c0': '  -  ',
 
@@ -40,11 +16,12 @@ def main():
 
                 'a2': '  -  ', 'b2': '|  -  |', 'c2': '  -  '
             }
+            self.winner = None
 
-        #Instance Method
+        # Instance Method
         def display_grid(self):
             """
-            Displays the repersentation of the current grid
+            Displays the visual representation of the current grid
             :return: Nothing
             """
             print(self.margin)
@@ -57,7 +34,7 @@ def main():
             print(f'{self.rows["a2"]}{self.rows["b2"]}{self.rows["c2"]}')
             print(self.margin)
 
-        def position_avaiable(self, pos):
+        def pos_available(self, pos):
             """
             If '-' is in selected position>> then position is available.
             If !'-' position is unavailable/ occupied.
@@ -65,18 +42,25 @@ def main():
             :return: True or False
             """
             if '-' in grid.rows[pos]:
-                print('Debug:position available ')
+                # print('Debug:position available ')
                 return True
             else:
-                print('Debug:position occupied')
+                # print('Debug:position occupied')
                 return False
 
         def place_pawn(self, pos, pawn):
+            """
+            Takes current players chosen position and Pawn, updates Grid Boxes.
+            :param pos: Player chosen position
+            :param pawn: Players Pawn
+            :return:
+            """
             self.rows[pos] = self.rows[pos].replace('-', pawn)
-            return  #display grid ?
+            return  # display grid ?
 
         def calculate_win(self, pawn):
             """
+            Takes player pawn attribute. Runs three functions to check for a winning combination
             :param pawn: player pawn
             :return: True or false
             """
@@ -87,122 +71,119 @@ def main():
             elif self.diagonal_win(pawn):
                 return True
             else:
-                print('No winning moves')
+                # print('No winning moves')
                 return False
 
-        def horizontal_win(self,pawn):
+        def horizontal_win(self, pawn):
             """
-            Condition 1: players pawn in the middle row(top,middle, bottom)>> else no horizontal win
-            Conditon 2: players pawn in both adjcent position from pervious middle>> else no horizontal win
-            :param pawn: current players pawn type. 'X' or 'O'
+            Condition 1:Pawn in the middle row(top, middle, bottom)
+            Conditon 2:Pawn in both adjacent position from previous middle
+            :param pawn: current players pawn 'X' or 'O'
             :return: True or False
             """
-            #top row check
+            # top row check
             if pawn in self.rows['b0']:
-                print('in top middle true ')
-                #now check outter edges
-                if pawn in self.rows['a0'] and pawn in  self.rows['c0']:
-                    print('out edges true ')
-                    print('horizontal win top row')
+                # print('in top middle true ')
+                # now check outter edges
+                if pawn in self.rows['a0'] and pawn in self.rows['c0']:
+                    # print('out edges true ')
+                    # print('horizontal win top row')
                     return True
                 else:
-                    print('Debug: No horizontal win')
+                    # print('Debug: No horizontal win')
                     return False
-            #Check row 2
+            # Check row 2
             elif pawn in self.rows['b1']:
-                print('in middle middle true ')
-                #now check outter edges
+                # print('in middle middle true ')
+                # now check outter edges
                 if pawn in self.rows['a1'] and pawn in self.rows['c1']:
-                    print('out edges true ')
-                    print('horizontal win middle row')
+                    # print('out edges true ')
+                    # print('horizontal win middle row')
                     return True
                 else:
-                    print('Debug: No horizontal win')
+                    # print('Debug: No horizontal win')
                     return False
-            #Check Bottom Row
+            # Check Bottom Row
             elif pawn in self.rows['b2']:
-                print('in bottom middle true ')
-                #now check outter edges
+                # print('in bottom middle true ')
+                # now check outter edges
                 if pawn in self.rows['a2'] and pawn in self.rows['c2']:
-                    print('out edges true ')
-                    print('horizontal win bottom row')
+                    # print('out edges true ')
+                    # print('horizontal win bottom row')
                     return True
                 else:
-                    print('Debug: No horizontal win')
+                    # print('Debug: No horizontal win')
                     return False
 
             else:
-                print('Debug: No horizontal win')
+                # print('Debug: No horizontal win')
                 return False
 
         def vertical_win(self, pawn):
             """
-            Condition 1: Player pawn must be present within the middle box of columns (a,b,c)
-            example a1, b1, c1>> if not no vertical win is possible
-            Condition 2: player pawn must be present at either verticle side of middle box
-            example pawn at 'a1'>>> pawns must be present at 'a0' and a2>> if not no vertical win is possible
-
-
+            Condition 1: Player pawn must be present within the middle box of columns (a Or b Or c)
+                            Example a1, b1, c1.
+            Condition 2: player pawn must be present at either vertical side of middle box
+                            Example pawn at 'a1' pawns must be present at 'a0' and a2>>
             :param pawn: player pawn
             :return: True or False
             """
-            #check middle position across board
+            # Check middle position across board
             if pawn in self.rows['a1']:
-                print('in col a  middle true ')
+                # print('in col a  middle true ')
                 if pawn in self.rows['a0'] and pawn in self.rows['a2']:
-                    print('top and bottom edges true')
-                    print('Vertical win true ')
+                    # print('top and bottom edges true')
+                    # print('Vertical win true ')
                     return True
                 else:
-                    print('No veritcal win')
+                    # print('No veritcal win')
                     return False
             elif pawn in self.rows['b1']:
-                print('in col b  middle true ')
+                # print('in col b  middle true ')
                 if pawn in self.rows['b0'] and pawn in self.rows['b2']:
-                    print('top and bottom edges true')
-                    print('Vertical win true ')
+                    # print('top and bottom edges true')
+                    # print('Vertical win true ')
                     return True
                 else:
-                    print('No veritcal win')
+                    # print('No veritcal win')
                     return False
             elif pawn in self.rows['c1']:
-                print('in col c  middle true ')
+                # print('in col c  middle true ')
                 if pawn in self.rows['c0'] and pawn in self.rows['c2']:
-                    print('top and bottom edges true')
-                    print('Vertical win true ')
+                    # print('top and bottom edges true')
+                    # print('Vertical win true ')
                     return True
                 else:
-                    print('No veritcal win')
+                    # print('No veritcal win')
                     return False
             else:
-                print('Debug: No Vertical win')
+                # print('Debug: No Vertical win')
                 return False
 
         def diagonal_win(self, pawn):
             """
-            Condition 1: player pawn must be present is middle box for any diagonal win:>>> if not return false
-            Conditon 2: player pawns must be present diagonal adjcent to middle boz to win: >>> if not return false
+            Condition 1: Pawn must be present in middle box for any diagonal win
+            Condition 2: Pawns must be present diagonal adjacent to middle boz to win
             :param pawn: player pawn
             :return: True or False
             """
-            # pawn in center box
+            #   pawn in center box
             if pawn in self.rows['b1']:
-                print('pawn in center box')
-                #Left to right diagonal
+                # print('pawn in center box')
+
+                #   Left to right diagonal
                 if pawn in self.rows['a0'] and pawn in self.rows['c2']:
-                    print('Diagonal winner left to right')
+                    # print('Diagonal winner left to right')
                     return True
                 elif pawn in self.rows['c0'] and pawn in self.rows['a2']:
-                    print('Diagonal winner right to left')
+                    # print('Diagonal winner right to left')
                     return True
                 else:
-                    print('no diagonal winner')
+                    # print('no diagonal winner')
                     return False
-
             else:
-                print('No diagonal Win')
+                # print('No diagonal Win')
                 return False
-
 
     class Player:
         def __init__(self):
@@ -217,72 +198,71 @@ def main():
         def deactivate(self):
             self.active = False
 
-    grid = Grid()
-    # player pawns
-    p1_pawn = 'X'
-    p2_pawn = '0'
-    # some form of user inpout
-    usr_pos = 'a0'
     game_on = True
-
-    round = 0
-    print('round', round)
-
+    game_menu_on = True
+    picks = 0
+    alpha = ['a', 'b', 'c']
+    numberic = ['0', '1', '2']
+    # Initialize Grid
+    grid = Grid()
+    # Initialize Players
     player1 = Player()
     player1.pawn = 'X'
     player2 = Player()
     player2.pawn = 'O'
 
-
-    #Welcome message
+    # Welcome message
     print('Welcome to TIC TAC TOE')
 
     while game_on:
-        print('round',round)
+        if picks % 2 == 0:
 
-        if round % 2 == 0:
-            player1.active = True
-            player2.active = False
+            print('Picks:', picks)
+            print('Player 1')
+            # player1.active = True
+            # player2.active = False
             grid.display_grid()
-            player1.position_selection = input('please select a box').lower()
-            #check if position ok
-            if grid.position_avaiable(pos=player1.position_selection):
-                # place pawn
-                grid.place_pawn(player1.position_selection, player1.pawn)
-                #show players the grid
-                grid.display_grid()
-                # see if move results in win
+            # Get player input
+            player1.pos_selection = input('Please select a box').lower()
+            len(player1.pos_selection) == 2
+
+            # Check if position available
+            if grid.pos_available(pos=player1.pos_selection):
+                # Place pawn
+                grid.place_pawn(player1.pos_selection, player1.pawn)
+                # Show updated grid
+                # Move result in win ?
                 if grid.calculate_win(player1.pawn):
+                    # Update winner
                     player1.winner = True
-                    print('Winner PLAYER 1')
+                    grid.winner = 'Player 1'
+                    # Update Game Status
                     game_on = False
                 else:
-                    round += 1
+                    picks += 1
                     # return to main loop
-
-
 
         else:
-            print('round', round)
-            player2.active = True
-            player1.active = False
+            print('Picks:', picks)
+            print('Player 2')
+            # player2.active = True
+            # player1.active = False
             grid.display_grid()
-
-            player2.position_selection = input('please select a box').lower()
-            # check if position ok
-            if grid.position_avaiable(pos=player2.position_selection):
-                # place pawn
+            player2.position_selection = input('Please select a box').lower()
+            if grid.pos_available(pos=player2.position_selection):
                 grid.place_pawn(player2.position_selection, player2.pawn)
-                # show players the grid
-                grid.display_grid()
-                # see if move results in win
                 if grid.calculate_win(player2.pawn):
                     player2.winner = True
-                    print('Winner PLAYER 1')
+                    grid.winner = 'Player 2'
                     game_on = False
                 else:
-                    round += 1
+                    picks += 1
                     # return to main loop
+    #   Exit messages
+    grid.display_grid()
+    print('Winner', grid.winner)
+
+
 
 if __name__ == "__main__":
     main()
